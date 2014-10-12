bae52c6e7bb709608b9a7d02f73626c3e16e990c
Adrian Hooper <hooper.adrian@gmail.com>
2014-10-12 12:17:26 -0400

    wrote code for firstInvocation method

--- setTimeout.js
+++ setTimeout.js
@@ -1,15 +1,21 @@
 (function(setTimeout, global) {
 	
+    // Why are we calling this interval? Shouldn't it be called delay?
 	var betterST = function(handler, interval, tags, manager) {
 		//var args = Array.prototype.slice.call(arguments, 0);
-		var rawHandle = setTimeout(handler, interval);
+        var invokedTime;
+		var rawHandle = setTimeout(function() {
+            invokedTime = new Date().getTime();
+            handler();
+        }, interval);
 		var called = new Date().getTime();
+
 		var handle = {
 			type: function(){ return "timeout";},
 			rawHandle : rawHandle,
 			clear: function(){ /* functionality to be added */},
             firstInvocation: function() {
-                /* functionality to be added */
+                return invokedTime;
             },
 			called: function(){
 				return called;
@@ -21,6 +27,7 @@
 		handle.remove = handle.clear;
 		return handle;
 	};
+
 	betterST.noConflict = function(){
 		global.setTimeout = setTimeout;
 		return betterST;
